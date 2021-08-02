09e75f54e5c9f56d043d384b2517599914ad9696
Svetlana Okuneva <yana.bogach@lenta.com>
2021-08-02 16:21:16 +0300

    HereClientInterface

---
+++ src/src/Client/HereClient.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Client;
+
+
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class HereClient implements HereClientInterface
+{
+
+    protected HttpClientInterface $client;
+    protected SerializerInterface $serializer;
+
+    /**
+     * HereClient constructor.
+     * @param HttpClientInterface $client
+     * @param SerializerInterface $serializer
+     */
+    public function __construct(HttpClientInterface $client, SerializerInterface $serializer)
+    {
+        $this->client = $client;
+        $this->serializer = $serializer;
+    }
+
+
+    public function sendRouteRequest()
+    {
+    }
+
+    public function getCalcRouteResult()
+    {
+    }
+}
---
+++ src/src/Client/HereClientInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Client;
+
+
+interface HereClientInterface
+{
+    public function sendRouteRequest();
+
+    public function getCalcRouteResult();
+}
