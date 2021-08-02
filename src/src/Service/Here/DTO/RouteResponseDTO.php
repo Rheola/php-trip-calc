d52a95a7f08c8a872a67e85707ff15cd90d1a66d
Svetlana Okuneva <yana.bogach@lenta.com>
2021-08-02 18:37:11 +0300

    RouteResponseDTO

---
+++ src/src/Service/Here/DTO/RouteResponseDTO.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Service\Here\DTO;
+
+
+class RouteResponseDTO
+{
+    protected string $id;
+
+    /**
+     * @var SectionResponseDTO[]
+     */
+    protected array $sections;
+}
