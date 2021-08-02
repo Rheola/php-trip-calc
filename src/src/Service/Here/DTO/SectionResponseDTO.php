594792df524e0fd4ed8126473b7177ca901a8a4e
Svetlana Okuneva <yana.bogach@lenta.com>
2021-08-02 18:36:39 +0300

    SectionResponseDTO

---
+++ src/src/Service/Here/DTO/SectionResponseDTO.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Service\Here\DTO;
+
+
+class SectionResponseDTO
+{
+    protected TravelSummaryResponseDTO $travelSummary;
+
+    /**
+     * @return TravelSummaryResponseDTO
+     */
+    public function getTravelSummary(): TravelSummaryResponseDTO
+    {
+        return $this->travelSummary;
+    }
+
+    /**
+     * @param TravelSummaryResponseDTO $travelSummary
+     */
+    public function setTravelSummary(TravelSummaryResponseDTO $travelSummary): void
+    {
+        $this->travelSummary = $travelSummary;
+    }
+}
