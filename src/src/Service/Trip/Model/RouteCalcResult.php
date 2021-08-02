5ef036392e90db04cf8a37b8eb976bfdeac13c91
Svetlana Okuneva <yana.bogach@lenta.com>
2021-08-02 16:19:36 +0300

    class RouteCalcResult

---
+++ src/src/Service/Trip/Model/RouteCalcResult.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Service\Trip\Model;
+
+
+class RouteCalcResult
+{
+    protected int $distance;
+    protected int $duration;
+
+    /**
+     * @return int
+     */
+    public function getDistance(): int
+    {
+        return $this->distance;
+    }
+
+    /**
+     * @param int $distance
+     */
+    public function setDistance(int $distance): RouteCalcResult
+    {
+        $this->distance = $distance;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDuration(): int
+    {
+        return $this->duration;
+    }
+
+    /**
+     * @param int $duration
+     */
+    public function setDuration(int $duration): RouteCalcResult
+    {
+        $this->duration = $duration;
+        return $this;
+    }
+}
