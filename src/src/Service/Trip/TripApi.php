5f783341ffe0569d208d6a5ecc4da15d541c2fc1
Svetlana Okuneva <yana.bogach@lenta.com>
2021-08-02 15:20:00 +0300

    TripApiInterface

---
+++ src/src/Service/Trip/TripApi.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Service\Trip;
+
+
+use App\Service\Trip\Model\RouteRequest;
+
+class TripApi implements TripApiInterface
+{
+
+    public function routeRequest(RouteRequest $request)
+    {
+    }
+
+    public function getResult(string $requestId)
+    {
+        // TODO: Implement getResult() method.
+    }
+}
---
+++ src/src/Service/Trip/TripApiInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Service\Trip;
+
+
+use App\Service\Trip\Model\RouteRequest;
+
+interface TripApiInterface
+{
+    public function routeRequest(RouteRequest $request);
+
+    public function getResult(string $requestId);
+}
