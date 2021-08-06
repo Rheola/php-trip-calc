24dd96c4e7f10a57f8275f423d792991e2487f09
Svetlana Okuneva <yana.bogach@lenta.com>
2021-08-06 11:07:21 +0300

    RouteResponseDTO

--- src/src/Service/Here/DTO/RouteResponseDTO.php
+++ src/src/Service/Here/DTO/RouteResponseDTO.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 
 namespace App\Service\Here\DTO;
@@ -12,4 +13,36 @@ class RouteResponseDTO
      * @var SectionResponseDTO[]
      */
     protected array $sections;
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return SectionResponseDTO[]
+     */
+    public function getSections(): array
+    {
+        return $this->sections;
+    }
+
+    /**
+     * @param SectionResponseDTO[] $sections
+     */
+    public function setSections(array $sections): void
+    {
+        $this->sections = $sections;
+    }
 }
