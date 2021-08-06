8aa7b45dfef0893fbd21743ca8cbf66d321fecf4
Svetlana Okuneva <yana.bogach@lenta.com>
2021-08-06 21:24:39 +0300

    TripApiInterface getResult

--- src/src/Service/Trip/TripApiInterface.php
+++ src/src/Service/Trip/TripApiInterface.php
@@ -1,12 +1,25 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Service\Trip;
 
-use App\Service\Trip\Model\RouteRequest;
+use App\Service\Trip\Model\RequestResponse;
+use App\Service\Trip\Model\ResultRequestModel;
+use App\Service\Trip\Model\RouteCalcResult;
+use App\Service\Trip\Model\RouteRequestModel;
 
 interface TripApiInterface
 {
-    public function routeRequest(RouteRequest $request);
+    /**
+     * @param RouteRequestModel $request
+     * @return RequestResponse
+     */
+    public function routeRequest(RouteRequestModel $request):RequestResponse;
 
-    public function getResult(string $requestId);
+
+    /**
+     * @param ResultRequestModel $request
+     * @return RouteCalcResult
+     */
+    public function getResult(ResultRequestModel $request):RouteCalcResult;
 }
