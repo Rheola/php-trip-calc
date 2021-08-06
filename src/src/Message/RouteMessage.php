1b459141037a5fa18a618ac2a2e6e6da6d2f0ab1
Svetlana Okuneva <yana.bogach@lenta.com>
2021-08-06 13:40:50 +0300

    class RouteMessage

---
+++ src/src/Message/RouteMessage.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\Message;
+
+
+class RouteMessage
+{
+    private $id;
+    private $context;
+
+
+    public function __construct(int $id, array $context = [])
+    {
+        $this->id = $id;
+        $this->context = $context;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContext()
+    {
+        return $this->context;
+    }
+
+    /**
+     * @param mixed $context
+     */
+    public function setContext($context): void
+    {
+        $this->context = $context;
+    }
+}
