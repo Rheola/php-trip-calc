80b1542600d900b001150058fb7fd9c70d83311a
Svetlana Okuneva <yana.bogach@lenta.com>
2021-08-07 08:36:43 +0300

    HereClient

--- src/src/Client/HereClient.php
+++ src/src/Client/HereClient.php
@@ -1,33 +1,81 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Client;
 
+use App\Service\Here\DTO\RouteQueryDTO;
+use App\Service\Here\DTO\RoutesResponseDTO;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class HereClient implements HereClientInterface
 {
 
     protected HttpClientInterface $client;
     protected SerializerInterface $serializer;
+    private string $apiUrl;
+    private string $apiKey;
+
 
     /**
      * HereClient constructor.
      * @param HttpClientInterface $client
      * @param SerializerInterface $serializer
+     * @param string              $hereApiUrl
+     * @param string              $hereApiKey
      */
-    public function __construct(HttpClientInterface $client, SerializerInterface $serializer)
-    {
+    public function __construct(
+        HttpClientInterface $client,
+        SerializerInterface $serializer,
+        string $hereApiUrl,
+        string $hereApiKey
+    ) {
         $this->client = $client;
         $this->serializer = $serializer;
+        $this->apiUrl = $hereApiUrl;
+        $this->apiKey = $hereApiKey;
     }
 
 
-    public function sendRouteRequest()
+    /**
+     * @param RouteQueryDTO $query
+     * @return RoutesResponseDTO
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function sendRouteRequest(RouteQueryDTO $query): RoutesResponseDTO
     {
-    }
+        $params = $query->toArray();
+        $params['apikey'] = $this->apiKey;
 
-    public function getCalcRouteResult()
-    {
+        $response = $this->client->request(
+            Request::METHOD_GET,
+            $this->apiUrl . '/routes?' . http_build_query($params),
+            [
+//                'headers' => $this->getHeaders(),
+                'timeout' => 600,
+            ]
+        );
+
+        if ($response->getStatusCode() == Response::HTTP_OK) {
+            $bodyResponseDTO = $this->serializer->deserialize(
+                $response->getContent(),
+                RoutesResponseDTO::class,
+                'json',
+                ['disable_type_enforcement' => true]
+            );
+
+            return $bodyResponseDTO;
+        }
+        throw new \Exception($response->getStatusCode());
     }
 }
