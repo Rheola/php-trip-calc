1ff6c861b1f723ba57e49339ea417737ece7d0a6
Svetlana Okuneva <yana.bogach@lenta.com>
2021-08-07 08:37:02 +0300

    RouteRequestModel

---
+++ src/src/Service/Trip/Model/RouteRequestModel.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service\Trip\Model;
+
+use App\Service\Here\DTO\RouteQueryDTO;
+
+class RouteRequestModel
+{
+    protected PointModel $from;
+
+    protected PointModel $to;
+
+    /**
+     * @return PointModel
+     */
+    public function getFrom(): PointModel
+    {
+        return $this->from;
+    }
+
+    /**
+     * @param PointModel $from
+     */
+    public function setFrom(PointModel $from): void
+    {
+        $this->from = $from;
+    }
+
+    /**
+     * @return PointModel
+     */
+    public function getTo(): PointModel
+    {
+        return $this->to;
+    }
+
+    /**
+     * @param PointModel $to
+     */
+    public function setTo(PointModel $to): void
+    {
+        $this->to = $to;
+    }
+
+    public function toRouteQueryDTO(): RouteQueryDTO
+    {
+        $from = $this->getFrom();
+        $to= $this->getTo();
+        $dto = new RouteQueryDTO();
+        $dto
+            ->setOrigin(sprintf('%d,%d', $from->getLat(), $from->getLon()))
+            ->setDestination(sprintf('%d,%d', $to->getLat(), $to->getLon()));
+        return $dto;
+    }
+}
