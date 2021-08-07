d49b0c7b94b15c50be48474ee0153cd898dfcab0
Svetlana Okuneva <yana.bogach@lenta.com>
2021-08-07 08:35:48 +0300

    RequestResponse

---
+++ src/src/Service/Trip/Model/RequestResponse.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+
+namespace App\Service\Trip\Model;
+
+
+class RequestResponse
+{
+    protected int $id;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+}
