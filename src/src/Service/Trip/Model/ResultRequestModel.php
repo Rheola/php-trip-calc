b8a2c239a4375c62ae7520245929d62a85049dc3
Svetlana Okuneva <yana.bogach@lenta.com>
2021-08-07 08:34:03 +0300

    ResultRequestModel

---
+++ src/src/Service/Trip/Model/ResultRequestModel.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Trip\Model;
+
+
+class ResultRequestModel
+{
+    protected $id;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+
+}
