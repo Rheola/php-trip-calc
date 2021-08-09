04071bcf815fb7f21e2882baf58fcfa4a126f14e
Svetlana Okuneva <yana.bogach@lenta.com>
2021-08-09 08:19:34 +0300

    инициализация

--- docker/init.sql
+++ docker/init.sql
@@ -1,12 +1,12 @@
 GRANT ALL PRIVILEGES ON DATABASE main TO main;
 
-CREATE ROLE testsym WITH PASSWORD 'testsym';
+CREATE ROLE trip_test_role WITH PASSWORD 'trip_test_password';
 
-create DATABASE testsym owner=testsym;
+create DATABASE trip_test owner=trip_test_role;
 
-ALTER ROLE testsym WITH LOGIN;
-ALTER USER testsym WITH SUPERUSER;
+ALTER ROLE trip_test_role WITH LOGIN;
+ALTER USER trip_test_role WITH SUPERUSER;
 
-GRANT ALL PRIVILEGES ON DATABASE main TO testsym;
+GRANT ALL PRIVILEGES ON DATABASE main TO trip_test_role;
 
-GRANT ALL PRIVILEGES ON DATABASE testsym TO testsym;
+GRANT ALL PRIVILEGES ON DATABASE trip_test TO trip_test_role;
